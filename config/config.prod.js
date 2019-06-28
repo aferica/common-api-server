849ec40b9b3df2ed9418218401b741a7d9d33000
aferica <18819466397@163.com>
2019-06-28 08:41:13 +0800

    修改配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -10,5 +10,30 @@ module.exports = () => {
 
   config.middleware = [];
 
+  config.redis = {
+    client: {
+      port: 6379,          // Redis port
+      host: '127.0.0.1',   // Redis host
+      password: 'afericaredis',
+      db: 0,
+    }
+  };
+
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: '127.0.0.1',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: 'aferica',
+      // database
+      database: 'common',    
+    }
+  };
+
   return config;
 };
